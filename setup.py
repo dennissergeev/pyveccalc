867336dc064bbd91a925a1f50d3e72179298bf50
Denis Sergeev <dennis.sergeev@gmail.com>
2015-04-26 23:57:43 +0100

    __init__.py and setup.py added

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Build and install the windspharm package."""
+from setuptools import setup
+
+for line in open('pyveccalc/__init__.py').readlines():
+    if line.startswith('__version__'):
+        exec(line)
+
+packages = ['pyveccalc']
+
+setup(name='pyveccalc',
+      version=__version__,
+      description='Wind vector calculations in finite differences',
+      author='Denis Sergeev',
+      author_email='d.sergeev@uea.ac.uk',
+      url='https://github.com/dennissergeev/pyveccalc',
+      long_description="""
+      Python library for wind vector calculations in finite differences
+      """,
+      packages=packages,
+      package_dir={'':'lib'},
+      install_requires=['numpy'],)
