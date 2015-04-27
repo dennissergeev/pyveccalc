cbd30d13c30eaba96cdea24dc7084e7c407ba823
Denis Sergeev <dennis.sergeev@gmail.com>
2015-04-27 01:04:22 +0100

    example script corrected

--- examples/wspd_vort_ex.py
+++ examples/wspd_vort_ex.py
@@ -25,7 +25,7 @@ with Dataset(example_data_path) as f:
 uu, info = prep_data(u,'tzyx')
 vv, _ = prep_data(v,'tzyx')
 
-W = WindHorizontal(uu,vv,lons,lats,'lonlat')
+W = WindHorizontal(uu,vv,lon,lat,'lonlat')
 
 rel_vort = W.vort_z()
 wspd = W.magnitude()
@@ -35,37 +35,37 @@ wspd = recover_data(wspd, info)
 
 # Pick a level and a moment in time
 # add a cyclic point (for plotting purposes)
-rel_vort_c, lons_c = addcyclic(rel_vort[0,0,:,:], lons)
-wspd_c, _ = addcyclic(wspd[0,0,:,:], lons)
+rel_vort_c, lon_c = addcyclic(rel_vort[0,0,:,:], lon)
+wspd_c, _ = addcyclic(wspd[0,0,:,:], lon)
 
 #
 # Plot wind speed and relative vorticity
 #
 fig, (ax1, ax2) = plt.subplots(nrows=2)
 
-m = Basemap(ax=ax1, projection='moll', resolution='c', \
-            llcrnrlon=0, llcrnrlat=-90, \
-            urcrnrlon=360.01, urcrnrlat=90)
-x, y = m(*np.meshgrid(lons_c, lats))
-c = m.contourf(x, y, wspd_c, cmap=plt.cm.hot_r,
+m = Basemap(ax=ax1, projection='cyl', resolution='c', \
+            llcrnrlon=0, llcrnrlat=-60, \
+            urcrnrlon=360.01, urcrnrlat=60)
+x, y = m(*np.meshgrid(lon_c, lat))
+c1 = m.contourf(x, y, wspd_c, cmap=plt.cm.hot_r,
                extend='both')
 m.drawcoastlines()
 m.drawparallels((-90, -60, -30, 0, 30, 60, 90), labels=[1,0,0,0])
 m.drawmeridians((0, 60, 120, 180, 240, 300, 360), labels=[0,0,0,1])
-plt.colorbar(c, ax=ax1, orientation='horizontal')
-ax1.title('Wind speed (ms$^{-1}$)', fontsize=16)
+plt.colorbar(c1, ax=ax1, orientation='horizontal')
+ax1.set_title('Wind speed (ms$^{-1}$)', fontsize=16)
 
-m = Basemap(ax=ax2, projection='moll', resolution='c', \
-            llcrnrlon=0, llcrnrlat=-90, \
-            urcrnrlon=360.01, urcrnrlat=90)
-x, y = m(*np.meshgrid(lons_c, lats))
-c = m.contourf(x, y, rel_vort_c*1e4, cmap=plt.cm.RdBu_r,
+m = Basemap(ax=ax2, projection='cyl', resolution='c', \
+            llcrnrlon=0, llcrnrlat=-60, \
+            urcrnrlon=360.01, urcrnrlat=60)
+x, y = m(*np.meshgrid(lon_c, lat))
+c2 = m.contourf(x, y, rel_vort_c*1e4, cmap=plt.cm.RdBu_r,
                extend='both')
 m.drawcoastlines()
 m.drawparallels((-90, -60, -30, 0, 30, 60, 90), labels=[1,0,0,0])
 m.drawmeridians((0, 60, 120, 180, 240, 300, 360), labels=[0,0,0,1])
-plt.colorbar(c, ax=ax1, orientation='horizontal')
-ax1.title('Relative vorticity ($10^{-4}$s$^{-1}$)', fontsize=16)
+plt.colorbar(c2, ax=ax2, orientation='horizontal')
+ax2.set_title('Relative vorticity ($10^{-4}$s$^{-1}$)', fontsize=16)
 
 fig.tight_layout()
-fig.show()
+plt.show()
